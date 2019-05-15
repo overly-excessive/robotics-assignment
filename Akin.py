d2e26a323d590f72d539e088ce00dfee134b7b23
overly-excessive <nirhu999@gmail.com>
2019-05-15 17:51:01 +0200

    made it tolerant to wrong answers by the user

--- Akin.py
+++ Akin.py
@@ -21,7 +21,9 @@ class Akin:
         # to store attributes not asked yet (all of them at the beginning)
         self.not_asked = self.attributes[:]
         # store previous answers. Each item corresponds to an animal and stores the number answers fitting this animal.
-        self.answers = [0]*len(self.animals) # all zeroes list by default
+        self.answers = [0]*len(self.animals) # all zeroes list by default, because no user answers yet
+        # store guesses made so far
+        self.guesses = []
 
     def get_question(self):
         # Returns the attribute to be asked about next, deletes it from the not asked yet list
@@ -44,7 +46,7 @@ class Akin:
                 best_attribute_index = at_index
 
         # assume we are gonna ask this one and delete it from the not asked yet list
-        del self.not_asked[best_attribute_index]
+        del self.not_asked[self.not_asked.index(self.attributes[best_attribute_index])]
 
         # return the attribute to be asked
         return self.attributes[best_attribute_index]
@@ -69,21 +71,45 @@ class Akin:
         else:
             raise ValueError
 
+    def guess(self):
+        # Returns a guess (the animal we think the user had in mind).
+        # Returns None if we dont know yet, i.e: more questions need to be asked from the user.
+
+        # First set answers if to zero if that particular animal has already been (incorrectly) guessed
+        for past_guess in self.guesses:
+            self.answers[self.animals.index(past_guess)] = 0
+
+        # Now get the list of possible solutions (for which the answer points are maximal)
+        possible_solutions = [i for i in range(len(A.answers)) if A.answers[i] == max(A.answers)]
+        if len(possible_solutions) == 1:    # If there is only one then return it
+            return self.animals[possible_solutions[0]]
+        else:   # otherwise return None (keep guessing)
+            return None
+
+    def wrong_guess(self, guess):
+        # This needs to be called when we guessed and the user said this wasn't it
+        self.guesses.append(guess)
 
 
-
-# TESTING
-A = Akin()
-
-while True:
-    solutions = len([i for i in range(len(A.answers)) if A.answers[i] == max(A.answers)])
-    if solutions == 1:
-        # print solution and break
-        answer = A.animals[A.answers.index(max(A.answers))]
-        print "Final answer: " + answer
-        break
-    else:
-        attribute_to_ask = A.get_question()
-        print attribute_to_ask
-        ans = raw_input("yes or no? ")
-        A.user_answer(attribute_to_ask, ans)
+# This is for testing but also serves as a guidance,
+# as in this is the game mechanic that needs to be implemented on pepper
+if __name__ == "__main__":
+    A = Akin()
+    while True:
+        solution = A.guess()
+        if solution:
+            # Ask user if this was it
+            yesorno = raw_input("I think you thought of " + solution + ". Was this it? (yes/no) ")
+            if yesorno == "yes":
+                print "I guessed it!"
+                break
+            elif yesorno == "no":
+                A.wrong_guess(solution)
+                continue
+            else:
+                raise ValueError
+        else:
+            attribute_to_ask = A.get_question()
+            print attribute_to_ask
+            ans = raw_input("(yes/no/dontknow) ")
+            A.user_answer(attribute_to_ask, ans)
